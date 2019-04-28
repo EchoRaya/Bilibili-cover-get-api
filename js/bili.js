a7f967c54f5ddb21ac67412cafb5d87f8d7ff655
SuperVBird <40413302+SuperVBird@users.noreply.github.com>
2019-04-28 22:03:43 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -5,26 +5,27 @@ window.onload = function(){
 				var reg=/^(av)?(AV)?\d{1,9}$/;
 				if(reg.test(avno)){
 					var iframe= document.getElementById("showPic");
-					//iframe.src="http://i2.hdslb.com/bfs/archive/191ee87533014f16aa3f32964858c6fb89e9d5de.png";
-					var url="https://search.bilibili.com/all?keyword="+avno+"&from_source=banner_search&spm_id_from=333.334.b_62616e6e65725f6c696e6b.1";
+					var url="https://bird.ioliu.cn/v1?url=https://search.bilibili.com/all?keyword="+avno+"&from_source=banner_search&spm_id_from=333.334.b_62616e6e65725f6c696e6b.1";
 					 $.ajax({
-							url:url,
-							type:"GET",
-							dataType:"html",
-							async:false,
-							success:function(result){
-								console.log(result);
-								//正则表达式获取body块
+						  url:url,
+						  type:'GET',                                
+						  dataType:'jsonp',                            
+						  success:function(result){
 								var reg = /[\s\S]*<\/body>/g;
 								var html = reg.exec(result)[0];
-								//然后用filter来筛选对应块对象，如：class='aa'
-								var aa = $(html).filter(".lazy-img");
-								var aahtml = aa.html();
-								console.log(aahtml);
-								//获取内容后可以插入当前html中。。。
-							}
-						});
-					iframe.style.display="block";
+								var reg2=/\"pic.*\.png\"/;
+								var key=reg2.exec(html)[0];
+								var key1=key.substring(7,key.length-1);
+								keystr = eval("'" + key1 + "'");
+								keystr = unescape(keystr.replace(/\u/g, "%u"));
+								////i2.hdslb.com/bfs/archive/191ee87533014f16aa3f32964858c6fb89e9d5de.png
+								iframe.src="http://"+keystr+"";
+							    iframe.style.display="block";
+						  },
+						  error:function(msg){
+							  alert(msg);                 
+						  }
+					 });	
 				}else{
 					alert("please input the right key");
 					window.location.reload();
